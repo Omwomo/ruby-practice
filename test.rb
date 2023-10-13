741646a8bf4fe5427465261dd48fbfda94e48145
Abdullah <geekyhacks22@gmail.com>
2023-10-13 20:29:05 +0300

    Add listing methods

---
+++ test.rb
@@ -0,0 +1,24 @@
+require_relative 'MyList'
+
+# Create our list
+list = MyList.new(1, 2, 3, 4)
+
+# Test #all?
+puts(list.all? { |e| e < 5 })
+# Output: true
+puts(list.all? { |e| e > 5 })
+# Output: false
+
+# Test #any?
+puts(
+  list.any? do |e|
+    e == 2
+  end
+)
+# Output: true
+puts(list.any? { |e| e == 5 })
+# Output: false
+
+# Test #filter
+puts(list.filter { |e| e.even? })
+# Output: [2, 4]
